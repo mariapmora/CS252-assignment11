32853e6022ee7e58a48e757542b6b302245812cf
cs_mmora021 <mmora021@odu.edu>
2024-04-22 22:47:54 -0400

    Completed assignment: fixed compilation errors and implemented test cases

--- src/cs252/TestMedian.java
+++ src/cs252/TestMedian.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class TestMedian {
 
-
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Supply the number of tests to run on the command line.");
@@ -17,14 +16,13 @@ public class TestMedian {
         }
     }
 
-
     private static boolean test(int numTests) {
         double[] inputs = { 1.0, 4.0, 6.5, 2.4, 24.0, 5.0 };
         double[] expected = { 1.0, 2.5, 4.0, 3.2, 4.0, 4.5 };
 
         for (int i = 0; i < numTests; ++i) {
-            double[] trimmedInputs = Arrays.copyOf(inputs, i+1);
-            Median med = new Median(trimmedInputs)
+            double[] trimmedInputs = Arrays.copyOf(inputs, i + 1);
+            Median med = new Median(trimmedInputs); // Added semicolon here
             double avg = med.median();
             if (avg != expected[i])
                 return false;
